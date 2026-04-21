dbcbd757ea14c4fc9589a3adb4920ea142af1b0d
aashraful349 <aashraful349@gmail.com>
2026-04-21 11:26:44 +0600

    made book card and called api to show data

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -2,10 +2,31 @@ import React, { use } from 'react';
 
 const Book = ({ singleBook }) => {
     // const data=use(bookPromise);
-    console.log(singleBook);
+    // console.log(singleBook);
+    const { bookName, author, image, category, rating, tags } = singleBook;
+    const [tag1, tag2] = tags;
     return (
-        <div>
-            <h1>Single Book</h1>
+        <div className="card bg-base-100 w-96 shadow-sm">
+            <figure className="p-3">
+                <img
+                    src={image}
+                    alt={bookName} />
+            </figure>
+            <div className="card-actions bg-[#23be0a0d] text-green-400 ml-5 mt-5">
+                <div className="badge badge-outline">{tag1}</div>
+                <div className="badge badge-outline">{tag2}</div>
+            </div>
+            <div className="card-body">
+                <h2 className="card-title">
+                    {bookName}
+
+                </h2>
+                <p>{`Author: ${author}`}</p>
+                <div className="card-actions justify-between">
+                    <div className="badge badge-outline">{category}</div>
+                    <div className="badge badge-outline">{rating}</div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -17,7 +17,9 @@ const Books = ({ data }) => {
             <h1 className='text-3xl text-center p-6'>Books</h1>
             <Suspense fallback={<span>Loading....</span>}>
                 {/* <Book bookPromise={bookPromise}></Book> */}
-                {data.map((singleBook)=><Book singleBook={singleBook} key={singleBook.bookId}></Book>)}
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+                    {data.map((singleBook)=><Book singleBook={singleBook} key={singleBook.bookId}></Book>)}
+                </div>
             </Suspense>
         </div>
     );
